327f8b22a8198e5143189d7387cc45e156cd59da
Chinmay Pathak <pathakchinmay02@gmail.com>
2023-08-15 23:17:33 +0530

    made some changes to the code

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         console.log(response.data);
       });
       setLocation("");
-    }
+    }   
   };
 
   return (
@@ -31,13 +31,13 @@ function App() {
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>{data.name}</p>
+            <p>{data?.name}</p>
           </div>
           <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+            {data?.main ? <h1>{data?.main.temp.toFixed()}°F</h1> : null}
           </div>
           <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
+            {data?.weather ? <p>{data?.weather[0].main}</p> : null}
           </div>
         </div>
 
@@ -45,17 +45,17 @@ function App() {
           <div className="bottom">
             <div className="feels">
               {data.main ? (
-                <p className="bold">{data.main.feels_like.toFixed()}°F</p>
+                <p className="bold">{data?.main.feels_like.toFixed()}°F</p>
               ) : null}
               <p>Feels Like</p>
             </div>
             <div className="humidity">
-              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+              {data.main ? <p className="bold">{data?.main.humidity}%</p> : null}
               <p>Humidity</p>
             </div>
             <div className="wind">
               {data.wind ? (
-                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+                <p className="bold">{data?.wind.speed.toFixed()} MPH</p>
               ) : null}
               <p>Wind Speed</p>
             </div>
